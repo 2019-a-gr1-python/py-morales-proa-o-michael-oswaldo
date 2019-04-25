3570554da716778532b2df8151de1b4d41c7a2af
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-04-24 21:49:38 -0500

    commit

--- VentaMascotas/ventaMascotas.py
+++ VentaMascotas/ventaMascotas.py
@@ -1,27 +1,43 @@
-print('Bienvenido a  "MyPet Store"')
 
-usuario = input('ingresa tu Password :');
+def crud():
+	print('\nBienvenido Administrador!')
+    print('1. Listar')
+    print('2. Crear')
+    print('3. Borrar')
+    print('4. Editar')
+    print('5. Salir')
+    accion = input('Que desea Hacer')
+    def listar():
+		print('Esta Listando')
+	def crear():
+		print('Esta crear')
+	def borrar():
+		print('Esta borrar')
+	def editar():
+		print('Esta editar')
+    def switch_accion():
+        return{
+            '1' :listar(),
+            '2' :crear(),
+            '3' :borrar(),
+             '4' :editar() 
+            }[accion]
+    return switch_accion()
 
 
-if usuario == 'admin':
-	crud();
-else:
-	print('no admin')	
+
+def adoptar():
+    print('aqui se adopta')
+    
+def tienda():    
+    print('Bienvenido a  "MyPet Store"')
+
+    usuario = input('ingresa tu Password :');
 
 
-def crud(){
-print('Bienvenido Administrador, que desea hacer?:')
-print('1. Listar')
-print('2. Crear')
-print('3. Borrar')
-print('4. Listar')
-print('5. Salir')
+    if usuario == 'admin':
+        crud()
+    else:
+        print('no admin')	
 
-}
-
-def adoptar(){
-print('aqui se adopta')
-
-}
-
-input('salir??')
+    input('salir??')
