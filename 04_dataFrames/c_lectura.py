bfc70ead2c89975a7ec85e1c991b5b3a975e8cd7
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-05-27 00:04:52 -0500

    finish dataframes

--- 04_dataFrames/c_lectura.py
+++ 04_dataFrames/c_lectura.py
@@ -12,10 +12,32 @@ import os #because we'll need to read things from OS
 #BINARY FILES --->  (ASDASFDASDFSSSSSSSSSSSSSDFSF)
 #Relational Databases 
 
-path = 'data/csv/artwork_data.csv'
-pd.read_csv(
-         'data/csv/artwork_data.csv',
+path = 'Documents/GitHub/py-morales-proa-o-michael-oswaldo/04_dataFrames/data/csv/artwork_data.csv'
+
+dir = os.getcwd()#get your directory work
+li = os.listdir(os.getcwd())
+df = pd.read_csv(
+         path,
         nrows = 5,
         usecols = ['id','artist'],
-        
+        index_col = 'id'
         )
+
+columnas_a_usar = ['id','artist','title','medium','year','acquisitionYear','height','width','units']
+
+
+df_completo = pd.read_csv(
+         path,
+        usecols = columnas_a_usar,
+        index_col = 'id'
+        )
+
+
+path_guardado = 'Documents/GitHub/py-morales-proa-o-michael-oswaldo/04_dataFrames/data/csv/artwork_data.pickle'
+
+
+
+
+df_completo.to_pickle(path_guardado)
+
+df_completo_pickle = pd.read_pickle(path_guardado)
--- 04_dataFrames/dataframes.py
+++ 04_dataFrames/dataframes.py
@@ -1,7 +1,40 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 24 14:23:30 2019
-
 @author: MichaelDrake
 """
+
+import pandas as pd
+import numpy as np
+
+arr_rand = np.random.randint(0,10,6).reshape(2,3)
+df = pd.DataFrame(arr_rand,columns = ['Estatura (cm)','Peso (gr)','Edad (years)'], index = ['Michael','Morales'])
+
+df2 = pd.DataFrame(arr_rand)
+df2.columns = ['Estatura (cm)','Peso (gr)','Edad (years)']
+
+#SOBREESCRIBIR LOS INDICES Y COLUMNAS
+df2.index = ['Michael','Morales']
+df3 = pd.DataFrame(arr_rand)
+
+df3[0]
+
+type(df['Estatura (cm)'])
+
+
+df_v = pd.DataFrame([[1,2,'Hello'],['Hello',4,5]])
+df_v[0]
+
+
+
+df['Peso (gr)']['Michael']
+
+
+
+
+
+
+
+
+
 
